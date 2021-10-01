6882396100d717feabf139e6d5b3b0edb8cdfe0e
Adam Kunzler <adamkunzler@gmail.com>
2021-10-01 01:01:16 -0500

    add more of the scene

--- RayTracerChallengeV2/main.cpp
+++ RayTracerChallengeV2/main.cpp
@@ -51,13 +51,13 @@ void RayTraceScene_Book();
 int main()
 {
 	std::cout << "\n\n --- Ray Tracer Challenge V2 --- \n\n";
-	
+
 	//benchmarkVector4(10000000); // 1.6s
 	//benchmarkMatrix4x4(10000000); // 6.7s
 	//benchmarkMatrix4x4Inversions(10000000); // 5.8s
 
 	RayTracer::Tests::RunTests();
-	
+
 	RayTraceScene_Book();
 
 	//Clock();
@@ -81,36 +81,14 @@ void DoSomething()
 	return;
 }
 
-/// <summary>
-/// Write a progress bar out to std::cout
-/// </summary>
-/// <param name="progress">a value between 0 and 1 inclusive</param>
-void showProgressBar(float progress)
-{
-	//https://stackoverflow.com/a/14539953
-
-	if (progress < 0) progress = 0;
-	if (progress > 1) progress = 1;
-
-	int barWidth = 70;
-
-	std::cout << "[";
-	int pos = (int)(barWidth * progress);
-	for (int i = 0; i < barWidth; ++i) {
-		if (i < pos) std::cout << "=";
-		else if (i == pos) std::cout << ">";
-		else std::cout << " ";
-	}
-	std::cout << "] " << int(progress * 100.0) << " %\r";
-	std::cout.flush();
-}
-
 void RayTraceScene_Book()
 {
 	std::cout << " --- Ray Trace Scene --- ";
-	
-	const int hsize = 1920*2;
-	const int vsize = 1080*2;
+
+	const int hsize = 192;
+	const int vsize = 108;
+	//const int hsize = 1920 * 1;
+	//const int vsize = 1080 * 1;
 	//const int hsize = 384 * 2;
 	//const int vsize = 216 * 2;
 	std::cout << hsize << " x " << vsize << " => " << (hsize * vsize) << " pixels\n\n";
@@ -122,8 +100,8 @@ void RayTraceScene_Book()
 	//
 	RayTracer::Camera camera(hsize, vsize, 1.152f);
 	camera.transform = RayTracer::viewTransform(
-		RayTracer::Vector4(-2.6f, 1.5f, -3.9f, 1.0f), 
-		RayTracer::Vector4(-0.6f, 1.0f, -0.8f, 1.0f), 
+		RayTracer::Vector4(-2.6f, 1.5f, -3.9f, 1.0f),
+		RayTracer::Vector4(-0.6f, 1.0f, -0.8f, 1.0f),
 		RayTracer::Vector4(0.0f, 1.0f, 0.0f, 0.0f));
 
 	//
@@ -132,29 +110,95 @@ void RayTraceScene_Book()
 	RayTracer::PointLight light(RayTracer::Vector4(-4.9f, 4.9f, -1.0f, 1.0f), RayTracer::Color(1.0f));
 	w.lights.push_back(light);
 
-	//
-	// floors, ceiling, and walls
-	//
-	RayTracer::Plane floor;
-	floor.transform = RayTracer::yRotation4x4(0.31415f);
-	RayTracer::CheckerPattern floorPattern(RayTracer::Color(0.35f, 0.35f, 0.35f), RayTracer::Color(0.65f, 0.65f, 0.65f));
-	floor.material.pattern = &floorPattern;
-	floor.material.specular = 0.0f;
-	floor.material.reflective = 0.4f;
-	w.objects.push_back(&floor);
+	{
+		//
+		// floors, ceiling, and walls
+		//
+		RayTracer::Plane floor;
+		floor.transform = RayTracer::yRotation4x4(0.31415f);
+		RayTracer::CheckerPattern floorPattern(RayTracer::Color(0.35f, 0.35f, 0.35f), RayTracer::Color(0.65f, 0.65f, 0.65f));
+		floor.material.pattern = &floorPattern;
+		floor.material.specular = 0.0f;
+		floor.material.reflective = 0.4f;
+		w.objects.push_back(&floor);
 
-	RayTracer::Sphere redSphere;
-	redSphere.transform = RayTracer::translation(-0.6f, 1.0f, 0.6f);
-	redSphere.material.color = RayTracer::Color(1.0f, 0.3f, 0.2f);
-	redSphere.material.specular = 0.4f;
-	redSphere.material.shininess = 5;
-	w.objects.push_back(&redSphere);
+		//
+		// Big Red Ball
+		//
+		RayTracer::Sphere redSphere;
+		redSphere.transform = RayTracer::translation(-0.6f, 1.0f, 0.6f);
+		redSphere.material.color = RayTracer::Color(1.0f, 0.3f, 0.2f);
+		redSphere.material.specular = 0.4f;
+		redSphere.material.shininess = 5;
+		w.objects.push_back(&redSphere);
+
+		//
+		// Glass Marbles
+		// 
+
+		RayTracer::Sphere blueMarble;
+		blueMarble.transform = RayTracer::translation(0.6f, 0.7f, -0.6f)
+			* RayTracer::scaling(0.7f, 0.7f, 0.7f);
+		blueMarble.material.color = RayTracer::Color(0, 0, 0.2f);
+		blueMarble.material.ambient = 0.0f;
+		blueMarble.material.diffuse = 0.4f;
+		blueMarble.material.specular = 0.9f;
+		blueMarble.material.shininess = 300;
+		blueMarble.material.reflective = 0.4f;
+		blueMarble.material.transparency = 0.9f;
+		blueMarble.material.refractiveIndex = 1.5f;
+		w.objects.push_back(&blueMarble);
+
+		RayTracer::Sphere greenMarble;
+		greenMarble.transform = RayTracer::translation(-0.7f, 0.5f, -0.8f)
+			* RayTracer::scaling(0.5f, 0.5f, 0.5f);
+		greenMarble.material.color = RayTracer::Color(0, 0.2f, 0);
+		greenMarble.material.ambient = 0.0f;
+		greenMarble.material.diffuse = 0.4f;
+		greenMarble.material.specular = 0.9f;
+		greenMarble.material.shininess = 300;
+		greenMarble.material.reflective = 0.9f;
+		greenMarble.material.transparency = 0.9f;
+		greenMarble.material.refractiveIndex = 1.5f;
+		w.objects.push_back(&greenMarble);
+
+		//
+		// Background balls
+		//
+		RayTracer::Sphere s1;
+		s1.transform = RayTracer::translation(4.6f, 0.4f, 1)
+			* RayTracer::scaling(0.4f, 0.4f, 0.4f);
+		s1.material.color = RayTracer::Color(0.8f, 0.5f, 0.3f);
+		s1.material.shininess = 50;
+		w.objects.push_back(&s1);
+
+		RayTracer::Sphere s2;
+		s2.transform = RayTracer::translation(4.7f, 0.3f, 0.4f)
+			* RayTracer::scaling(0.3f, 0.3f, 0.3f);
+		s2.material.color = RayTracer::Color(0.9f, 0.4f, 0.5f);
+		s2.material.shininess = 50;
+		w.objects.push_back(&s2);
+
+		RayTracer::Sphere s3;
+		s3.transform = RayTracer::translation(-1.0f, 0.5f, 4.5f)
+			* RayTracer::scaling(0.5f, 0.5f, 0.5f);
+		s3.material.color = RayTracer::Color(0.4f, 0.9f, 0.6f);
+		s3.material.shininess = 50;
+		w.objects.push_back(&s3);
+
+		RayTracer::Sphere s4;
+		s4.transform = RayTracer::translation(-1.7f, 0.3f, 4.7f)
+			* RayTracer::scaling(0.3f, 0.3f, 0.3f);
+		s4.material.color = RayTracer::Color(0.4f, 0.6f, 0.9f);
+		s4.material.shininess = 50;
+		w.objects.push_back(&s4);
+	}
 
 	// run the ray tracer...
 	auto start = std::chrono::high_resolution_clock::now();
 
 	RayTracer::Canvas image = render(camera, w);
-	
+
 	auto stop = std::chrono::high_resolution_clock::now();
 	auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
 
